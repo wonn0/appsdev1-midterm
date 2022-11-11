57d7cf92a97a2ed208bca1f49bf16f5b5a886769
DESKTOP-OCNRPM2\Ron Matthew Alunan <ronalunan@gmail.com>
2022-11-11 17:21:28 +0800

    implemented hotfixes to css

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,14 +1,28 @@
-import React from "react"
-import './Item.css'
+import React from "react";
+import "./Item.css";
 
 const Item = ({ name, onEdit, onDelete }) => {
-    return (
-        <div className="item-container">
-            <p className="item-name">{name}</p>
-            <button type="button" className="item-button edit-button" onClick={onEdit}>Edit</button>
-            <button type="button" className="item-button delete-button" onClick={onDelete}>Delete</button>
-        </div>
-    )
-}
+  return (
+    <div className="item-container">
+      <h2>{name}</h2>
+      <div className="button-container">
+        <button
+          type="button"
+          className="item-button edit-button"
+          onClick={onEdit}
+        >
+          Edit
+        </button>
+        <button
+          type="button"
+          className="item-button delete-button"
+          onClick={onDelete}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Item;
